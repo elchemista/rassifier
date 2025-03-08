c12c2d5784f44635d261d17d4d1d084180ae3456
Yuriy Zhar <darkyfelis@gmail.com>
2025-03-09 00:37:59 +0100

    added checksum

---
+++ checksum-Elixir.Rassifier.exs
@@ -0,0 +1,11 @@
+%{
+  "librassifier-v0.1.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:909069408157f801f88197d512a3a217543491db917f22a4b4af30ad757c0ed3",
+  "librassifier-v0.1.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:0e67d986ce8871f829ed8d37126bea558638cac9e558fca207a827847e7e189e",
+  "librassifier-v0.1.1-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:8649609f9d220d8c36016e5c5d5a8a0d8d4a622cc409a3d042ab1946121c1a54",
+  "librassifier-v0.1.1-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:5fdd384203bba65a59943c3f1a79de571cb4b9663109e8fc0e91188db192d439",
+  "librassifier-v0.1.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:ba973d22ebf370444908667e5b3e7161356229d8f7d168c12796e16fa7582488",
+  "librassifier-v0.1.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:4527d2042c6bdbc737e02f9c9c9e7d20fe5864679218b2a9275b078330b3fced",
+  "librassifier-v0.1.1-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:448c2284c0a730a2f6e88c56413f7308c52c5a358d73405c0d74166f7ca3a94f",
+  "rassifier-v0.1.1-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:5c2507efe2bf4406b1b8c6fcb6d7e624c2394105049ac784773e74bfdff282bb",
+  "rassifier-v0.1.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:5b6eca7a7ae9397ae910f885b8fd698df19eb4cb704491e584f197eb3e7a7be3",
+}
